422781b62ce4d1c68b750a0bbc9eb68f30b0d52b
Shashang Bhagat <shbhagat@bestpeers.com>
2020-05-12 20:39:17 +0530

    configured cors

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var path = require('path')
 var cookieParser = require('cookie-parser')
 var logger = require('morgan')
 var bodyParser = require('body-parser')
+var cors = require('cors')
 
 var indexRouter = require('./routes/index')
 const connect = require('./dbConfig')
@@ -17,6 +18,7 @@ app.use(cookieParser())
 app.use(express.static(path.join(__dirname, 'public')))
 app.use(bodyParser.json({ limit: '5mb' }))
 app.use(bodyParser.urlencoded({ extended: false }))
+app.use(cors('*'))
 
 app.use('/api', indexRouter)
 
